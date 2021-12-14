e5c0337b06825b1b796b1b3b22947f5a890e276b
titiscms <titisbackup@gmail.com>
2021-12-14 20:43:55 -0300

    Custom cascading delete implentation

--- src/main/java/com/ead/course/repositories/LessonRepository.java
+++ src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -2,8 +2,14 @@ package com.ead.course.repositories;
 
 import com.ead.course.models.LessonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
+
+    @Query(value = "SELECT * FROM tb_lessons l WHERE l.module_module_id = :moduleId",nativeQuery = true)
+    List<LessonModel> findAllLessonIntoModule(@Param("moduleId") UUID moduleId);
 }
